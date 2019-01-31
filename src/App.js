1d7229bd6660e0ddd4283b5e802dfac1054146e0
Maxime Dulin <maximedulin@MacBook-Pro-de-Maxime.local>
2019-01-31 13:10:18 +0100

    Add sub components for forms and tools to compartimentaze everything
    Signed-off-by: Maxime Dulin <maximedulin@MacBook-Pro-de-Maxime.local>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { ProgressBar } from './Components/ProgressBar/ProgressBar';
-import { RadioButton } from './Components/RadioButton/RadioButton';
 import { getQuestionList } from './API/FetchData';
+import { QuestionForm } from './Components/QuestionForm/QuestionForm';
+import { OutcomeForm } from './Components/OutcomeForm/OutcomeForm';
+import { Header } from './Components/Header/Header';
 
 import './App.css';
 
@@ -89,75 +90,34 @@ class App extends Component {
   _getProgressBarPercentage = () => {
     const questionIndex = this.state.currentQuestion ? this.state.questions.map(e => e.id ).indexOf(this.state.currentQuestion.id) : -1;
     if (questionIndex === -1) {
+      if (this.state.finalOutcome) {
+        return 100;
+      }
       return 0;
     } else {
       return (questionIndex / this.state.questions.length) * 100;
     }
   }
 
-  _displayQuestionsForm = () => {
-    const { currentQuestion, currentAnswer } = this.state;
-    return (
-      currentQuestion ? (
-      <div className="Question-form">
-        <p>{ currentQuestion.question_text }</p>
-        <svg width="40" height="5">
-          <rect width="40" height="3" rx="1" ry="1" className="rect-under-question-text" />
-        </svg>
-        <div className="radio-button-container">
-          {
-            currentQuestion.answers.map(ans => {
-              return (<RadioButton key={ans.id} currentAnswer={currentAnswer} ans={ans} handleOptionChange={this.handleOptionChange}/>)
-            })
-          }
-        </div>
-        <button disabled={!currentAnswer} className={!currentAnswer ? 'next-button-position global-button disabled-button' : 'next-button-position global-button'} onClick={this.goToNextQuestion}>
-          <span>Next</span>
-          <svg className="next-button-icon" width="9px" height="16px" viewBox="0 0 9 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-              <g id="icons/ic-arrow-right-white" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                  <path d="M6.58629142,8.00500036 L0.292640816,1.70685429 C-0.0977440278,1.3161906 -0.0975179782,0.68302566 0.293145712,0.292640816 C0.683809402,-0.0977440278 1.31697434,-0.0975179782 1.70735918,0.293145712 L8.70735918,7.29814571 C9.0975468,7.68861203 9.09754696,8.32138741 8.70735954,8.71185393 L1.71735954,15.7068539 C1.3269749,16.0975178 0.693809962,16.0977442 0.303146073,15.7073595 C-0.0875178163,15.3169749 -0.0877441892,14.68381 0.302640455,14.2931461 L6.58629142,8.00500036 Z" id="Shape" fill={!currentAnswer ? '#7B99A9' : '#FFFFFF'} fillRule="nonzero"></path>
-              </g>
-          </svg>
-          </button>
-    </div>) : (<div className="App"></div>))
-  }
-
-  _displayOutcomeForm = () => {
-    const { finalOutcome } = this.state;
-    return (
-      <div className="outcome-form">
-        <h2>Thank you for answering the questions!</h2>
-        <svg width="40" height="5">
-          <rect width="40" height="3" rx="1" ry="1" className="rect-under-question-text" />
-        </svg>
-        <p>{ finalOutcome.text }</p>
-        { finalOutcome.show_booking_button ?
-          (<button className="global-button">Book a meeting</button>) : (<div></div>)}
-        <span className="restart-button" onClick={this.restartProcess}>Back to the start screen</span>
-      </div>
-    )
-  }
-
   render() {
-    const { finalOutcome, questionQueue } = this.state;
+    const { finalOutcome, questionQueue, currentQuestion, currentAnswer } = this.state;
     return (
-      <div className="App">
-      <div className="App-header">
-        {
-          (questionQueue.length > 0 && !finalOutcome) ?
-          (<button className="previous-button" onClick={this.goToPreviousQuestion}>
-            <svg width="9px" height="16px" viewBox="0 0 9 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                <g id="icons/ic-arrow-left-green" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <path d="M6.58629142,8.00500036 L0.292640816,1.70685429 C-0.0977440278,1.3161906 -0.0975179782,0.68302566 0.293145712,0.292640816 C0.683809402,-0.0977440278 1.31697434,-0.0975179782 1.70735918,0.293145712 L8.70735918,7.29814571 C9.0975468,7.68861203 9.09754696,8.32138741 8.70735954,8.71185393 L1.71735954,15.7068539 C1.3269749,16.0975178 0.693809962,16.0977442 0.303146073,15.7073595 C-0.0875178163,15.3169749 -0.0877441892,14.68381 0.302640455,14.2931461 L6.58629142,8.00500036 Z" id="Shape" fill="#69CDBB" fillRule="nonzero" transform="translate(4.500000, 8.000000) rotate(-180.000000) translate(-4.500000, -8.000000) "></path>
-                </g>
-            </svg>
-          </button>)
-          : (<div></div>)
+      <div className="app">
+        <Header progressBarPercentage={this._getProgressBarPercentage()}
+                questionQueue={questionQueue}
+                finalOutcome={finalOutcome}
+                goToPreviousQuestion={this.goToPreviousQuestion}/>
+        { !finalOutcome ?
+          (
+            <QuestionForm currentQuestion={currentQuestion}
+                          currentAnswer={currentAnswer}
+                          handleOptionChange={this.handleOptionChange}
+                          goToNextQuestion={this.goToNextQuestion}/>
+          ) : (
+            <OutcomeForm restartProcess={this.restartProcess}
+                         finalOutcome={finalOutcome}/>
+          )
         }
-        <p>Heartburn Checker</p>
-        <ProgressBar percentage={this._getProgressBarPercentage()}/>
-      </div>
-        { !finalOutcome ? this._displayQuestionsForm() : this._displayOutcomeForm() }
       </div>
     );
   }
---
+++ src/Components/OutcomeForm/OutcomeForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './OutcomeForm.css'
+import { CustomButton } from '../CustomButton/CustomButton';
+
+export class OutcomeForm extends Component {
+
+  bookApointment = () => {
+    console.log('Do booking stuff')
+  }
+
+  render () {
+    const { finalOutcome, restartProcess } = this.props;
+    return (
+      <div className="outcome-form">
+        <h2>Thank you for answering the questions!</h2>
+        <svg width="40" height="5">
+          <rect width="40" height="3" rx="1" ry="1" className="rect-under-question-text" />
+        </svg>
+        <p>{ finalOutcome.text }</p>
+        { finalOutcome.show_booking_button ?
+          (<CustomButton isDisabled={false} clickEvent={this.bookApointment} buttonText="Book an meeting"/>) : (<div></div>)}
+        <span className="restart-button" onClick={restartProcess}>Back to the start screen</span>
+      </div>
+    );
+  }
+}
